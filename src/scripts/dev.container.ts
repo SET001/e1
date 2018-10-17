33bc30fb74f2365d6bda6c4783241cd3b88f9095
SET001 <settydark@gmail.com>
2018-10-17 22:59:54 +0300

    up

---
+++ src/scripts/dev.container.ts
@@ -0,0 +1,25 @@
+import { Container } from "inversify"
+import { Game } from './core/game'
+import * as Systems from './systems'
+
+class Action{
+  state: boolean = false
+  constructor(public name: string, public keys: string[]){}
+}
+// import { PhysicService, CannonPhysicService } from './services/physic'
+
+const container = new Container()
+container.bind<any>('Game').to(Game)
+container.bind<any>('SystemsList').toConstantValue([
+	new Systems.TickIncome(),
+	new Systems.TickOutcome(),
+	new Systems.KeyboardInput([
+		new Action('left', ['KeyA', 'ArrowLeft', 'Numpad4']),
+		new Action('right', ['KeyD', 'ArrowDown', 'Numpad6']),
+		new Action('up', ['KeyW', 'ArrowUp', 'Numpad8']),
+		new Action('down', ['KeyS', 'ArrowDown', 'Numpad5']),
+	])
+])
+
+// const actions = 
+export { container }
--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,33 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {App} from './app'
-import * as ReduxThunk from 'redux-thunk'
 import { Provider }  from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import reducers from './reducers'
-import { Game } from "./game";
-import { KeyboardController, Action } from './controller'
-const store = createStore(reducers)
-store.dispatch({type: 'tick'})
+import { container } from './dev.container'
 
-const game = new Game(store)
-const actions = [
-  new Action('left', ['KeyA', 'ArrowLeft', 'Numpad4']),
-  new Action('right', ['KeyD', 'ArrowDown', 'Numpad6']),
-  new Action('up', ['KeyW', 'ArrowUp', 'Numpad8']),
-  new Action('down', ['KeyS', 'ArrowDown', 'Numpad5']),
-]
-const controller =  new KeyboardController(
-  store, actions
-  
-)
-controller.init()
+const game = container.get<any>('Game');
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={game.store}>
     <App />
   </Provider>,
   document.getElementById('appContainer')
 );
 
 game.run()
+
+// import { createStore, applyMiddleware } from 'redux'
+// import reducers from './reducers'
+// import { Game } from "./game";
+// import { KeyboardController, Action } from './controller'
+// const store = createStore(reducers)
+// store.dispatch({type: 'tick'})
+
+// const game = new Game(store)
+// const actions = [
+//   new Action('left', ['KeyA', 'ArrowLeft', 'Numpad4']),
+//   new Action('right', ['KeyD', 'ArrowDown', 'Numpad6']),
+//   new Action('up', ['KeyW', 'ArrowUp', 'Numpad8']),
+//   new Action('down', ['KeyS', 'ArrowDown', 'Numpad5']),
+// ]
+// const controller =  new KeyboardController(
+//   store, actions
+  
+// )
+// controller.init()
