7196ccca51705d3944f348afabf27e369f35582b
SET001 <settydark@gmail.com>
2018-10-22 23:46:41 +0300

    code style fix

--- src/scripts/actions/index.ts
+++ src/scripts/actions/index.ts
@@ -1,4 +1,4 @@
-import { Building } from '../entities'
+import { Building } from '../entities';
 import { ThunkDispatch } from 'redux-thunk';
 import { RootState } from '../state';
 import { shouldSpawn, randomCreature } from '../systems';
@@ -10,27 +10,28 @@ export const udpateResources = (gold: number) => ({ type:'udpateResources', payl
 
 // 	render
 export const addRenderObject = (object: any, layer: string) => ({
-  type:'addRenderObject',
   layer,
+  type:'addRenderObject',
   payload: object});
 
 // buildings
 export const addBuilding = (building: Building) => ({ type: 'addBuilding' });
 export const startBuilding = (building: Building) => ({ type:'startBuilding', payload: building });
 export const cancelBuilding = () => ({ type:'cancelBuilding' });
-export const successBuilding = () => (dispatch: ThunkDispatch<any, any, any>, getState:getState) => {
-  const state = getState();
-  const { building } = state.buildingCursor;
-  const { gold } = state.resources;
-  console.log('success building', building, getState());
-  return Promise.all([
-    dispatch(addRenderObject(state.buildingCursor.building.sprite, 'buildings')),
-    dispatch(udpateResources(gold - building.cost)),
-    dispatch(addBuilding(building)),
-  ]).then(() => dispatch({
-    type:'successBuilding',
-  }));
-};
+export const successBuilding = () =>
+  (dispatch: ThunkDispatch<any, any, any>, getState:getState) => {
+    const state = getState();
+    const { building } = state.buildingCursor;
+    const { gold } = state.resources;
+    console.log('success building', building, getState());
+    return Promise.all([
+      dispatch(addRenderObject(state.buildingCursor.building.sprite, 'buildings')),
+      dispatch(udpateResources(gold - building.cost)),
+      dispatch(addBuilding(building)),
+    ]).then(() => dispatch({
+      type:'successBuilding',
+    }));
+  };
 
 export const tick = () => (dispatch: ThunkDispatch<any, any, any>, getState: getState) => {
 	// dispatch(spawnCreature())
